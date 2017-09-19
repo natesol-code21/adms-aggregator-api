26dbb089df53b0d3cd54f8974f3dfe77de8df640
nsolomon <nsolomon@adx.lab76.org>
2017-09-19 06:56:05 -0400

    #updating...

--- test/feed.test.js
+++ test/feed.test.js
@@ -20,18 +20,33 @@ describe('Feed', function() {
       });
   });
 
-  // it('should get a single feeds', function(done) {
-  //   api.get('/feeds/findOne')
-  //     .expect('Content-Type', /json/)
-  //     .expect(200)
-  //     .end(function(err, res) {
-  //       if (err) {
-  //         return done(err);
-  //       }
-  //       var feeds = res.body;
-  //
-  //       expect(Array.isArray(feeds)).to.be.false;
-  //       done();
-  //     });
-  // });
+  it('should get a single feeds', function(done) {
+    api.get('/feeds/findOne')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        var feeds = res.body;
+        expect(Array.isArray(feeds)).to.be.false;
+        done();
+      });
+  });
+
+
+    it('should get total number of feeds', function(done) {
+      api.get('/feeds/count')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+          var feeds = res.body;
+          expect(Array.isArray(feeds)).to.be.false;
+          done();
+        });
+    });
+
 });
