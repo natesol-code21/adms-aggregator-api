663a50a1fa9bb4e6355a971ebf3b166e14312448
nsolomon <nsolomon@adx.lab76.org>
2017-09-25 14:27:16 -0400

    #updating testing...

--- test/feed.test.js
+++ test/feed.test.js
@@ -21,6 +21,28 @@ describe('Feeds', function() {
         done();
    });
  });
+
+ it('should add a SINGLE feed on /api/feeds POST', function(done) {
+   chai.request(server)
+     .post('/api/feeds')
+     .send({'name': 'USEIA Feed', 'title': 'USEIA Feed Publication', 'description':'This is USEIA Feed publication','feed_url':'http://useia.com'})
+     .end(function(err, res){
+       console.log(res.body);
+       res.should.have.status(200);
+       res.should.be.json;
+       res.body.should.be.a('object');
+       res.body.should.have.property('name');
+       res.body.should.have.property('title');
+       res.body.should.have.property('description');
+       res.body.should.have.property('feed_url');
+       res.body.name.should.equal('USEIA Feed');
+       res.body.title.should.equal('USEIA Feed Publication');
+       res.body.description.should.equal('This is USEIA Feed publication');
+       res.body.feed_url.should.equal('http://useia.com');
+       done();
+     });
+ });
+
   it('should list ALL feeds on /api/feeds GET', function(done) {
   chai.request(server)
     .get('/api/feeds')
