7499fe9cc1aa143d1ccdb49b23c61346a46ac6b8
nsolomon <nsolomon@adx.lab76.org>
2017-09-24 21:35:04 -0400

    #updating

--- test/feed.test.js
+++ test/feed.test.js
@@ -7,11 +7,27 @@ chai.use(chaiHttp);
 
 
 describe('Feeds', function() {
+
+  it('Post a new feed', function (done) {
+     chai.request(server)
+     .post('/api/feeds').send({
+        name: 'cfr feed',
+        title: 'CFR',
+        description: 'This is cfr feed publication',
+        feed_url: 'http://cfr.feed.com'
+      })
+      .end(function(err, res){
+        res.should.have.status(200);
+        done();
+   });
+ });
   it('should list ALL feeds on /api/feeds GET', function(done) {
   chai.request(server)
     .get('/api/feeds')
     .end(function(err, res){
       res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
       done();
     });
 });
